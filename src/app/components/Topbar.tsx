514790abd7a2ed2b79a5f0931925bc09058b79ff
leeyuan <dbdks32123@gmail.com>
2024-01-29 18:45:23 +0900

    레이아웃 탑바 컴포넌트

---
+++ src/app/components/Topbar.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import React, { useState, useEffect } from "react";
+import styles from "@/app/styles/topbar.module.scss";
+import Link from "next/link";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+const links = [
+  { name: 'Search', href: '/Search' },
+  { name: 'Algorithm', href: '/Algorithm' },
+  { name: 'Map', href: '/Map' }
+];
+
+
+function Topbar() {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [name, setName] = useState<string | JwtPayload>('');
+
+  useEffect(() => {
+    const loadUserFromToken = () => {
+      const token = localStorage.getItem('token');
+
+      if (token) {
+        const decodedToken = jwt.decode(token) as JwtPayload;
+
+        if (decodedToken) {
+          const userName = decodedToken.name;
+          setIsLoggedIn(true);
+          setName(userName);
+        }
+      } else {
+        setIsLoggedIn(false);
+        setName('');
+      }
+    };
+
+    loadUserFromToken();
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+    setName('');
+    window.location.href = '/';
+  };
+
+
+  return (
+    <div className={styles.topbar}>
+      <div className={styles.div1}>{links.map((link) => (
+        <Link
+          key={link.name}
+          href={link.href}
+        >
+          <p className={`${styles.linkname}`}>{link.name}</p>
+        </Link>
+      ))}</div>
+      <div className={styles.div2}>
+        <Link href="/" >MUMAP</Link>
+      </div>
+      <div className={styles.div3}>
+        {isLoggedIn ? (
+          <>
+            <p>{`${name} 님`}</p>
+            <Link href="/mypage/myinfo" >MYPAGE</Link>
+            <button className={styles.link} onClick={handleLogout}>LOGOUT</button>
+          </>
+        ) : (
+          <>
+            <Link className={styles.link} href="/signup">SIGN UP</Link>
+            <Link className={styles.link} href="/login">LOGIN</Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
