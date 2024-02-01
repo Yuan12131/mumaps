bd12a6b8e348ba0b71157f40cc399bc064e3600b
leeyuan <dbdks32123@gmail.com>
2024-02-01 11:06:41 +0900

    chore : 로그인 링크

--- src/app/components/Topbar.tsx
+++ src/app/components/Topbar.tsx
@@ -1,25 +1,26 @@
-'use client'
+"use client";
 
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import styles from "@/app/styles/topbar.module.scss";
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import LoginButton from "./Login";
+import { useRouter } from "next/navigation";
 
 const links = [
-  { name: 'Search', href: '/search' },
-  { name: 'Algorithm', href: '/algorithm' },
-  { name: 'Map', href: '/map' }
+  { name: "Search", href: "/search" },
+  { name: "Algorithm", href: "/algorithm" },
+  { name: "Map", href: "/map" },
 ];
 
-
 function Topbar() {
-  
+  const router = useRouter();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [name, setName] = useState<string | JwtPayload>('');
+  const [name, setName] = useState<string | JwtPayload>("");
 
   useEffect(() => {
     const loadUserFromToken = () => {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
 
       if (token) {
         const decodedToken = jwt.decode(token) as JwtPayload;
@@ -31,7 +32,7 @@ function Topbar() {
         }
       } else {
         setIsLoggedIn(false);
-        setName('');
+        setName("");
       }
     };
 
@@ -39,45 +40,41 @@ function Topbar() {
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     setIsLoggedIn(false);
-    setName('');
-    window.location.href = '/';
+    setName("");
+    window.location.href = "/";
   };
-
 
   return (
     <div className={styles.topbar}>
-
-      <div className={styles.div1}>{links.map((link) => (
-        <Link
-          key={link.name}
-          href={link.href}
-        >
-          <p className={styles.linkname}>{link.name}</p>
-        </Link>
-      ))}
+      <div className={styles.div1}>
+        {links.map((link) => (
+          <Link key={link.name} href={link.href}>
+            <p className={styles.linkname}>{link.name}</p>
+          </Link>
+        ))}
       </div>
 
       <div className={styles.div2}>
-        <Link href="/" >MUMAPS</Link>
+        <Link href="/">MUMAPS</Link>
       </div>
 
       <div className={styles.div3}>
         {isLoggedIn ? (
           <>
             <p>{`${name} 님`}</p>
-            <Link href="/mypage/myinfo" >MYPAGE</Link>
-            <button className={styles.link} onClick={handleLogout}>LOGOUT</button>
+            <Link href="/mypage/myinfo">MYPAGE</Link>
+            <button className={styles.link} onClick={handleLogout}>
+              LOGOUT
+            </button>
           </>
         ) : (
           <>
-            <Link className={styles.link} href="/signup">SIGN UP</Link>
-            <Link className={styles.link} href="/login">LOGIN</Link>
+            <Link href="/login">login</Link>
           </>
         )}
       </div>
-
     </div>
   );
 }
