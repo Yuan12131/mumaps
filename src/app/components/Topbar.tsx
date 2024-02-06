01beb013edc51936cf016042d81265b8f9b2e626
leeyuan <dbdks32123@gmail.com>
2024-02-06 13:13:54 +0900

    chore

--- src/app/components/Topbar.tsx
+++ src/app/components/Topbar.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import styles from "@/app/styles/topbar.module.scss";
-import WebPlayback from "./Webplayback";
 
 const links = [
   { name: "SEARCH", href: "/search" },
@@ -54,7 +53,6 @@ function Topbar() {
         ) : (
           <>
             <button className={styles.link}>LOGOUT</button>
-            <WebPlayback token={token} />
           </>
         )}
       </div>
