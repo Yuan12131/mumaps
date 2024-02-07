267c822417322366f8fb55d7ae98d5c8b1d25915
leeyuan <dbdks32123@gmail.com>
2024-02-07 19:00:17 +0900

    device_id 연결

--- src/app/components/Webplayback.tsx
+++ src/app/components/Webplayback.tsx
@@ -3,7 +3,6 @@
 
 import React, { useState, useEffect, useRef } from "react";
 import ProgressBar from "./ProgressBar";
-import VolumeBar from "./VolumeBar";
 import styles from "@/app/styles/player.module.scss";
 import { useRouter } from "next/navigation";
 
@@ -21,7 +20,6 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
   const [current_track, setTrack] = useState(track);
   const [duration, setDuration] = useState(1);
   const [position, setPosition] = useState(0);
-  const [volume, setVolume] = useState(0);
   const previousDeviceIdRef = useRef<string | null>(null);
 
   useEffect(() => {
@@ -39,13 +37,40 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
         },
         volume: 0.5,
       });
-    
+
       setPlayer(player);
 
       player.addListener("ready", async ({ device_id }) => {
+        // console.log("Ready event - Device ID:", device_id);
+      
+        // // 추가적인 디바이스 정보 출력
+        // const devicesResponse = await fetch(
+        //   "https://api.spotify.com/v1/me/player/devices",
+        //   {
+        //     method: "GET",
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //       Authorization: `Bearer ${props.token}`,
+        //     },
+        //   }
+        // );
+      
+        // if (!devicesResponse.ok) {
+        //   console.error("Failed to fetch devices:", devicesResponse.statusText);
+        //   return;
+        // }
+      
+        // const devicesData = await devicesResponse.json();
+        // console.log("Available devices:", devicesData.devices);
+      
+        // // 활성화 및 비활성화된 디바이스 설정
+        // for (const device of devicesData.devices) {
+        //   device.is_active = device.id === device_id;
+        // }
+      
         // 이전 트랙이 있다면 정리
         player.removeListener("ready");
-
+      
         // 새로운 트랙을 재생
         const trackId = props.trackId;
         const response = await fetch(
@@ -62,21 +87,25 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
             }),
           }
         );
-
+      
         if (response.ok) {
           console.log("Track played successfully");
         } else {
-          console.error("Failed to play track", response.statusText);
+          console.error("Failed to play track:", response.statusText);
         }
-
+      
         // 디바이스 아이디 저장
         previousDeviceIdRef.current = device_id;
+        console.log("Current Device ID:", device_id);
       });
+      
+      
 
-      player.addListener("player_state_changed", (state) => {
+      player.addListener('player_state_changed', (state) => {
         if (!state) {
           return;
         }
+        console.log(state);
         setTrack(state.track_window.current_track);
         setPaused(state.paused);
         setPosition(state.position);
@@ -93,7 +122,8 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
 
       player.addListener("authentication_error", ({ message }) => {
         console.log(message);
-        alert("스포티파이 로그인 후 재생 할 수 있습니다.");
+        alert("스포티파이 로그인 후 이용해주세요");
+        window.location.href = "/login";
       });
     };
 
@@ -107,8 +137,8 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
           <img
             src={current_track.album.images[0].url}
             alt="album"
-            width={64}
-            height={64}
+            width={50}
+            height={50}
           />
           <div>
             <p className={styles.trackName}>{current_track.name}</p>
@@ -126,40 +156,12 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
       <div className={styles.control}>
         <div className={styles.playback}>
           <img
-            src="/images/prev_song_arrow.svg"
-            alt="previous"
-            width={36}
-            height={36}
-            onClick={() => {
-              player?.previousTrack();
-              setPosition(0);
-            }}
-          />
-          <img
             src={`/images/${is_paused ? "play" : "pause"}_circle.svg`}
             alt={is_paused ? "play" : "pause"}
-            width={36}
-            height={36}
+            width={30}
+            height={30}
             onClick={() => player?.togglePlay()}
           />
-          <img
-            src="/images/next_song_arrow.svg"
-            alt="previous"
-            width={36}
-            height={36}
-            onClick={() => {
-              player?.nextTrack();
-              setPosition(0);
-            }}
-          />
-          <button
-            className="btn-spotify"
-            onClick={() => {
-              player?.togglePlay();
-            }}
-          >
-            {is_paused ? "PLAY" : "PAUSE"}
-          </button>
         </div>
         <ProgressBar
           is_paused={is_paused}
@@ -169,15 +171,6 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
           onSeek={(position) => {
             player?.seek(position);
             setPosition(position);
-          }}
-        />
-      </div>
-      <div className={styles.option}>
-        <VolumeBar
-          volume={volume}
-          onSeek={() => {
-            player?.setVolume(volume);
-            setVolume(volume);
           }}
         />
       </div>
