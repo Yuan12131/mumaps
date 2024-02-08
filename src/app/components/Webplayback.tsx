fe6e23e62b95eb41bb87d8f390b7dc6c0005ac47
leeyuan <dbdks32123@gmail.com>
2024-02-08 15:45:43 +0900

    refactoring

--- src/app/components/Webplayback.tsx
+++ src/app/components/Webplayback.tsx
@@ -4,13 +4,12 @@
 import React, { useState, useEffect, useRef } from "react";
 import ProgressBar from "./ProgressBar";
 import styles from "@/app/styles/player.module.scss";
-import { useRouter } from "next/navigation";
 
 const track = {
   name: "",
   album: {
     images: [{ url: "" }],
-  },
+  },      
   artists: [{ name: "" }],
 };
 
@@ -21,6 +20,7 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
   const [duration, setDuration] = useState(1);
   const [position, setPosition] = useState(0);
   const previousDeviceIdRef = useRef<string | null>(null);
+  
 
   useEffect(() => {
     const script = document.createElement("script");
@@ -31,7 +31,7 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
 
     window.onSpotifyWebPlaybackSDKReady = async () => {
       const player = new window.Spotify.Player({
-        name: "Web Playback SDK",
+        name: "MUMAPS",
         getOAuthToken: (cb) => {
           cb(props.token);
         },
@@ -41,36 +41,9 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
       setPlayer(player);
 
       player.addListener("ready", async ({ device_id }) => {
-        // console.log("Ready event - Device ID:", device_id);
-      
-        // // 추가적인 디바이스 정보 출력
-        // const devicesResponse = await fetch(
-        //   "https://api.spotify.com/v1/me/player/devices",
-        //   {
-        //     method: "GET",
-        //     headers: {
-        //       "Content-Type": "application/json",
-        //       Authorization: `Bearer ${props.token}`,
-        //     },
-        //   }
-        // );
-      
-        // if (!devicesResponse.ok) {
-        //   console.error("Failed to fetch devices:", devicesResponse.statusText);
-        //   return;
-        // }
-      
-        // const devicesData = await devicesResponse.json();
-        // console.log("Available devices:", devicesData.devices);
-      
-        // // 활성화 및 비활성화된 디바이스 설정
-        // for (const device of devicesData.devices) {
-        //   device.is_active = device.id === device_id;
-        // }
-      
         // 이전 트랙이 있다면 정리
         player.removeListener("ready");
-      
+
         // 새로운 트랙을 재생
         const trackId = props.trackId;
         const response = await fetch(
@@ -92,20 +65,16 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
           console.log("Track played successfully");
         } else {
           console.error("Failed to play track:", response.statusText);
+        alert('Spotify 디바이스에 MUMAPS를 연결하고 다시 재생해주세요.')
         }
       
         // 디바이스 아이디 저장
         previousDeviceIdRef.current = device_id;
-        console.log("Current Device ID:", device_id);
       });
       
       
 
-      player.addListener('player_state_changed', (state) => {
-        if (!state) {
-          return;
-        }
-        console.log(state);
+      player.addListener("player_state_changed", (state) => {
         setTrack(state.track_window.current_track);
         setPaused(state.paused);
         setPosition(state.position);
@@ -154,15 +123,6 @@ function WebPlayback(props: { token: string | null; trackId: string }) {
         </div>
       )}
       <div className={styles.control}>
-        <div className={styles.playback}>
-          <img
-            src={`/images/${is_paused ? "play" : "pause"}_circle.svg`}
-            alt={is_paused ? "play" : "pause"}
-            width={30}
-            height={30}
-            onClick={() => player?.togglePlay()}
-          />
-        </div>
         <ProgressBar
           is_paused={is_paused}
           position={position}
