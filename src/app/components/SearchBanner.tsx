a12398f64399e9c7d5af82a6d1968078de63b2cc
leeyuan <dbdks32123@gmail.com>
2024-02-15 21:04:41 +0900

    main : 검색 배너 컴포넌트

---
+++ src/app/components/SearchBanner.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "@/app/styles/searchbanner.module.scss";
+import Link from "next/link";
+
+function SearchBanner() {
+
+
+  return (
+          <div className={styles.banner}>
+        <Link href="/search">
+          <div className={styles.form}>🔍 지금 바로 Spotify에 검색하기</div>
+        </Link>
+      </div>
+  );
+}
+
+export default SearchBanner;
